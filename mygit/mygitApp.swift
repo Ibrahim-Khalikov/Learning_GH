004d6296f56c0b3a25866dc12b17fa9bdb5f2867
Ibrahim Khalikov <starlink050@gmail.com>
2021-05-22 16:24:02 +0500

    Initial Commit

---
+++ mygit/mygitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mygitApp.swift
+//  mygit
+//
+//  Created by cloud_vfx on 22/05/21.
+//
+
+import SwiftUI
+
+@main
+struct mygitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
